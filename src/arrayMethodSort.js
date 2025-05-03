4117dd7cf29f6d1fa5bf864059acb14a08862db5
Nikita Ovod <stiffovod@gmail.com>
2025-05-03 21:43:55 +0300

    Return positive, negative or zero by compareFunction instead of boolean value

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,8 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function (
-    compareFunction = (a, b) => String(a) > String(b),
+    compareFunction = (a, b) =>
+      String(a) > String(b) ? 1 : String(a) < String(b) ? -1 : 0,
   ) {
     for (let i = 0; i < this.length - 1; i++) {
       for (let j = 0; j < this.length - i - 1; j++) {
